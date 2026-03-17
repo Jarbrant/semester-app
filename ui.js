0a6d9d7f9c013bea8f7bc588b465c96c4e79f88f
Jarbrant <andersmenyit@gmail.com>
2026-03-17 18:00:26 +0100

    Add functions to open and close modals

--- ui.js
+++ ui.js
@@ -1,1 +1,9 @@
+window.openModal = function(id) {
+    const modal = document.getElementById(id);
+    if (modal) modal.classList.add("show");
+};
 
+window.closeModal = function() {
+    document.querySelectorAll(".modal")
+        .forEach(m => m.classList.remove("show"));
+};
