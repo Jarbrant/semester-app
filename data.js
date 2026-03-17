70f5eb483de09ce17c3cad011176f74cfa440cdf
Jarbrant <andersmenyit@gmail.com>
2026-03-17 17:57:56 +0100

    Add functions to manage employees and vacations

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
+function getEmployees() {
+    return JSON.parse(localStorage.getItem("employees")) || [];
+}
 
+function saveEmployees(data) {
+    localStorage.setItem("employees", JSON.stringify(data));
+}
+
+function getVacations() {
+    return JSON.parse(localStorage.getItem("vacations")) || [];
+}
+
+function saveVacations(data) {
+    localStorage.setItem("vacations", JSON.stringify(data));
+}
